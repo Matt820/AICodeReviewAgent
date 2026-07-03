e5024c1053c82addca7bb7cb821cd225db4260a7
Dalinn Sanchez <dalinn.sanchez@gmail.com>
2026-07-02 22:49:01 -0500

    -

--- src/AiCodeReviewAgent.Cli/obj/Debug/net10.0/AiCodeReviewAgent.Cli.AssemblyInfo.cs
+++ src/AiCodeReviewAgent.Cli/obj/Debug/net10.0/AiCodeReviewAgent.Cli.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AiCodeReviewAgent.Cli")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d364e55c303581cc8b8fa4d8ec9541cfeb4c1ba7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e3bde892c8758334763cade66c1b3a92acc07a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("AiCodeReviewAgent.Cli")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AiCodeReviewAgent.Cli")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
